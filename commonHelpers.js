136c19f0322522eb46433f03e1428ad4e2155015
MagdalenaAntkowiak <130896720+MagdalenaAntkowiak@users.noreply.github.com>
2024-07-18 18:41:42 +0000

    Deploying to gh-pages from @ MagdalenaAntkowiak/goit-js-hw-10-ver.2@daf683fa76b6f3064a60d18c47a8bece954be6b8 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";import{f as e}from"./assets/vendor-2b44ac2e.js";document.addEventListener("DOMContentLoaded",function(){e("#datetime-picker",{enableTime:!0,dateFormat:"Y-m-d H:i"})});
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{f as t}from"./assets/vendor-2b44ac2e.js";const o={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){console.log(e[0])}};t("#datetime-picker",o);
 //# sourceMappingURL=commonHelpers.js.map
